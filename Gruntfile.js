61c14058851cc8ea20d2ec61461df8d37e82ed72
Nadin Nadin <nadin.ts@gmail.com>
2015-06-21 18:32:33 +0300

    автоматизировала сборку css из less

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        less: {
+            style: {
+                files: {
+                    'css/style.css': 'less/**/*.less'
+                }
+            }
+        },
+
+        watch: {
+            css: {
+                files: 'less/**/*.less',
+                tasks: ['less'],
+                options: {
+                    livereload: true
+                }
+            }
+        }
+    });
+};
